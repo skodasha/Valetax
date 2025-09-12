c0fb7de3ec4c0ccd68dee729b057d11a5f6acce8
DariaSkobeleva <skodarya@gmail.com>
2025-09-12 08:54:40 +0200

    update app structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <h1>Valetax</h1>
+    </div>
+  );
+};
+
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@/assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+  },
 });
